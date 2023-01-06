6500692619af540cdb9c7478726351e393c1f3a8
mylittlecorner <83501465+mylittlecorner@users.noreply.github.com>
2023-01-06 22:45:21 +0100

    3 files modified, 6 added

--- src/app/modules/input-page/input-page.module.ts
+++ src/app/modules/input-page/input-page.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { InputboxComponent } from './inputbox/inputbox.component';
+import { DownloadButtonComponent } from './download-button/download-button.component';
 
 @NgModule({
+  declarations: [MainComponent, InputboxComponent, DownloadButtonComponent],
   imports: [CommonModule],
   exports: [MainComponent],
-  declarations: [MainComponent],
 })
 export class InputPageModule {}
